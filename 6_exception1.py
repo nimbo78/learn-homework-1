297ac1a3d6d83a0696226b7e99ecd5076b1f56b4
Igor Singosin <igor.sin.78@gmail.com>
2020-09-12 13:09:03 +0300

    6 done

--- 6_exception1.py
+++ 6_exception1.py
@@ -14,7 +14,21 @@ def ask_user():
     """
     Замените pass на ваш код
     """
-    pass
-    
+    words = ask_user_dict()
+    try:
+      while True:
+        user_say = input('Пользователь: ')
+        user_input = words.get(user_say)
+        if user_input is not None:
+          print(f'Программа: {user_input}')
+        else:
+          break
+    except KeyboardInterrupt:
+      print("\nПрограмма: Пока!")
+
+def ask_user_dict():
+  words = {"Как дела": "Хорошо!", "Что делаешь?": "Программирую"}
+  return words
+
 if __name__ == "__main__":
     ask_user()
